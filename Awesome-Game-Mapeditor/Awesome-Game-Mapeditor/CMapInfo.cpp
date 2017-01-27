4d9b1a81f4cecad4db51de40c9bff135869d3120
Spikeone <fvv1@gmx.de>
2017-01-27 18:28:57 +0100

    MapInfo is serialized and saved to a file

--- Awesome-Game-Mapeditor/Awesome-Game-Mapeditor/CMapInfo.cpp
+++ Awesome-Game-Mapeditor/Awesome-Game-Mapeditor/CMapInfo.cpp
@@ -75,7 +75,7 @@ bool CMapInfo::hasVersion()
 
 CMapInfo::CMapInfo()
 {
-    this->strMapname = "New World";
+    this->strMapname = "File has been saved";
     this->uiSize[0] = 0;
     this->uiSize[1] = 0;
     
@@ -85,10 +85,69 @@ CMapInfo::CMapInfo()
 
 std::uint8_t* CMapInfo::serialize()
 {
+    // Mapinfo block size
     unsigned long int ulData = 0;
+    unsigned long int ulOffset = 0;
 
+    // Data length = 2 Bytes
+    ulData += 2;
+
+    // Mapnamelength = length
+    // no sizeof here!
+    ulData += this->strMapname.length();
+
+    // sizeof sizex = 2 Bytes
     ulData += sizeof(this->uiSize[0]);
+
+    // sizeof sizey = 2 Bytes
+    ulData += sizeof(this->uiSize[1]);
+
+    // sizeof version = 2 Bytes
+    ulData += sizeof(this->uiVersion);
     std::cout << "Data: " << ulData << std::endl;
+
+    // Datalength
+    unsigned char* ucData = new unsigned char[ulData];
+    ucData[ulOffset] = (int)ulData / 256;
+    ucData[ulOffset + 1] = ulData - ((int)ulData / 256) * 256;
+    ulOffset += 2;
+
+    // SizeX
+    ucData[ulOffset] = (int)this->uiSize[0] / 256;
+    ucData[ulOffset + 1] = this->uiSize[0] - ((int)this->uiSize[0] / 256) * 256;
+    ulOffset += 2;
+
+    // SizeY
+    ucData[ulOffset] = (int)this->uiSize[1] / 256;
+    ucData[ulOffset + 1] = this->uiSize[1] - ((int)this->uiSize[1] / 256) * 256;
+    ulOffset += 2;
+
+    //Version
+    ucData[ulOffset] = (int)this->uiVersion / 256;
+    ucData[ulOffset + 1] = this->uiVersion - ((int)this->uiVersion / 256) * 256;
+    ulOffset += 2;
+
+    unsigned long int ulBuffer = this->strMapname.length();
+    unsigned char* trap = new unsigned char[this->strMapname.length()];
+
+    char* destString = new char[this->strMapname.length()];
+    strcpy_s(destString, this->strMapname.length() + 1, this->strMapname.c_str());
+
+    for (int i = 0; i < this->strMapname.length(); i++)
+    {
+        ucData[ulOffset] = destString[i];
+        ulOffset++;
+    }
+
+    std::fstream tempmapsave;
+    char savestring[60] = "Maps\\mapfile.awf";
+    //strncat_s(savestring, ".\Maps\mapfile.awf", 29);
+    tempmapsave.open(savestring, std::ios_base::binary | std::ios_base::out);
+    for (unsigned long int j = 0; j <= ulData - 1; j++)
+    {
+        tempmapsave << ucData[j];
+    }
+    tempmapsave.close();
 
     return nullptr;
 }
